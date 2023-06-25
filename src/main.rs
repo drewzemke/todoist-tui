9b7b7167e81c31cb6d3b7ba5cd7ac0476281e77b
Drew Zemke <drew.zemke@gmail.com>
2023-06-24 22:37:34 -0700

    chore: more POC hacking for sync api

--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,47 @@ async fn main() {
     let file = fs::read_to_string(&auth_path).unwrap();
     let config: Config = toml::from_str(file.as_str()).unwrap();
 
-    println!("home: {}", auth_path.to_str().unwrap());
-    println!("api_key: {}", config.api_key);
+    // println!("home: {}", auth_path.to_str().unwrap());
+    // println!("api_key: {}", config.api_key);
 
-    // get_projects().await;
+    // get_projects(config.api_key).await;
+    get_user(config.api_key).await;
 }
 
-pub async fn get_projects() {
+#[derive(Debug, Deserialize)]
+struct SyncResponse {
+    user: User,
+}
+
+#[derive(Debug, Deserialize)]
+struct User {
+    full_name: String,
+    inbox_project_id: String,
+}
+
+pub async fn get_user(api_key: String) {
+    let sync_url = "https://api.todoist.com/sync/v9/sync";
+
+    let mut map = HashMap::new();
+    map.insert("sync_token", "*");
+    map.insert("resource_types", "[\"user\"]");
+
+    let client = reqwest::Client::new();
+    let resp = match client
+        .post(sync_url)
+        .header("Authorization", format!("Bearer {}", api_key))
+        .json(&map)
+        .send()
+        .await
+    {
+        Ok(resp) => resp.json::<SyncResponse>().await.unwrap(),
+        Err(err) => panic!("Error: {}", err),
+    };
+
+    dbg!(resp.user);
+}
+
+pub async fn get_projects(api_key: String) {
     let sync_url = "https://api.todoist.com/sync/v9/sync";
 
     let mut map = HashMap::new();
